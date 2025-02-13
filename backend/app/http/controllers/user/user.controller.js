c3ff677b3ffabdd592450a6bf4aa23902da61733
Ali <86645460+AliMoradpour@users.noreply.github.com>
2025-02-13 20:15:57 +0330

    change website name in backend

--- backend/app/http/controllers/user/user.controller.js
+++ backend/app/http/controllers/user/user.controller.js
@@ -14,11 +14,7 @@ const CODE_EXPIRES = 90 * 1000; //90 seconds in miliseconds
 const { StatusCodes: HttpStatus } = require("http-status-codes");
 const path = require("path");
 const { ROLES } = require("../../../../utils/constants");
-const {
-  checkOtpSchema,
-  completeProfileSchema,
-  updateProfileSchema,
-} = require("../../validators/user/user.schema");
+const { checkOtpSchema, completeProfileSchema, updateProfileSchema } = require("../../validators/user/user.schema");
 const { PaymentModel } = require("../../../models/payment");
 
 class userAuthController extends Controller {
@@ -30,8 +26,7 @@ class userAuthController extends Controller {
   async getOtp(req, res) {
     let { phoneNumber } = req.body;
 
-    if (!phoneNumber)
-      throw createError.BadRequest("شماره موبایل معتبر را وارد کنید");
+    if (!phoneNumber) throw createError.BadRequest("شماره موبایل معتبر را وارد کنید");
 
     phoneNumber = phoneNumber.trim();
     this.phoneNumber = phoneNumber;
@@ -47,10 +42,7 @@ class userAuthController extends Controller {
     await checkOtpSchema.validateAsync(req.body);
     const { otp: code, phoneNumber } = req.body;
 
-    const user = await UserModel.findOne(
-      { phoneNumber },
-      { password: 0, refreshToken: 0, accessToken: 0 }
-    );
+    const user = await UserModel.findOne({ phoneNumber }, { password: 0, refreshToken: 0, accessToken: 0 });
     // .populate([
     //   {
     //     path: "Products",
@@ -74,8 +66,7 @@ class userAuthController extends Controller {
 
     if (!user) throw createError.NotFound("کاربری با این مشخصات یافت نشد");
 
-    if (user.otp.code != code)
-      throw createError.BadRequest("کد ارسال شده صحیح نمیباشد");
+    if (user.otp.code != code) throw createError.BadRequest("کد ارسال شده صحیح نمیباشد");
 
     if (new Date(`${user.otp.expiresIn}`).getTime() < Date.now())
       throw createError.BadRequest("کد اعتبار سنجی منقضی شده است");
@@ -86,9 +77,8 @@ class userAuthController extends Controller {
     // await setAuthCookie(res, user); // set httpOnly cookie
     await setAccessToken(res, user);
     await setRefreshToken(res, user);
-    let WELLCOME_MESSAGE = `کد تایید شد، به فرانت هوکس خوش آمدید`;
-    if (!user.isActive)
-      WELLCOME_MESSAGE = `کد تایید شد، لطفا اطلاعات خود را تکمیل کنید`;
+    let WELLCOME_MESSAGE = `کد تایید شد، به نکست شاپ خوش آمدید`;
+    if (!user.isActive) WELLCOME_MESSAGE = `کد تایید شد، لطفا اطلاعات خود را تکمیل کنید`;
 
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
@@ -119,13 +109,9 @@ class userAuthController extends Controller {
   }
   async updateUser(phoneNumber, objectData = {}) {
     Object.keys(objectData).forEach((key) => {
-      if (["", " ", 0, null, undefined, "0", NaN].includes(objectData[key]))
-        delete objectData[key];
+      if (["", " ", 0, null, undefined, "0", NaN].includes(objectData[key])) delete objectData[key];
     });
-    const updatedResult = await UserModel.updateOne(
-      { phoneNumber },
-      { $set: objectData }
-    );
+    const updatedResult = await UserModel.updateOne({ phoneNumber }, { $set: objectData });
     return !!updatedResult.modifiedCount;
   }
   sendOTP(phoneNumber, res) {
@@ -144,9 +130,7 @@ class userAuthController extends Controller {
           return res.status(HttpStatus.OK).send({
             statusCode: HttpStatus.OK,
             data: {
-              message: `کد تائید برای شماره موبایل ${toPersianDigits(
-                phoneNumber
-              )} ارسال گردید`,
+              message: `کد تائید برای شماره موبایل ${toPersianDigits(phoneNumber)} ارسال گردید`,
               expiresIn: CODE_EXPIRES,
               phoneNumber,
             },
@@ -164,15 +148,11 @@ class userAuthController extends Controller {
     const { user } = req;
     const { name, email } = req.body;
 
-    if (!user.isVerifiedPhoneNumber)
-      throw createError.Forbidden("شماره موبایل خود را تایید کنید.");
+    if (!user.isVerifiedPhoneNumber) throw createError.Forbidden("شماره موبایل خود را تایید کنید.");
 
     const duplicateUser = await UserModel.findOne({ email });
 
-    if (duplicateUser)
-      throw createError.BadRequest(
-        "کاربری با این ایمیل قبلا ثبت نام کرده است."
-      );
+    if (duplicateUser) throw createError.BadRequest("کاربری با این ایمیل قبلا ثبت نام کرده است.");
 
     const updatedUser = await UserModel.findOneAndUpdate(
       { _id: user._id },
@@ -202,8 +182,7 @@ class userAuthController extends Controller {
         $set: { name, email, biography, phoneNumber },
       }
     );
-    if (!updateResult.modifiedCount === 0)
-      throw createError.BadRequest("اطلاعات ویرایش نشد");
+    if (!updateResult.modifiedCount === 0) throw createError.BadRequest("اطلاعات ویرایش نشد");
     return res.status(HttpStatus.OK).json({
       statusCode: HttpStatus.OK,
       data: {
@@ -247,8 +226,7 @@ class userAuthController extends Controller {
       sameSite: "Lax",
       secure: true,
       path: "/",
-      domain:
-        process.env.NODE_ENV === "development" ? "localhost" : ".fronthooks.ir",
+      domain: process.env.NODE_ENV === "development" ? "localhost" : ".fronthooks.ir",
     };
     res.cookie("accessToken", null, cookieOptions);
     res.cookie("refreshToken", null, cookieOptions);
