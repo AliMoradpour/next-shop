3f1cb927344daab8eee750bc5260da5413909836
Ali <86645460+AliMoradpour@users.noreply.github.com>
2025-02-17 23:32:35 +0330

    style textField comp

--- frontend/src/common/TextField.jsx
+++ frontend/src/common/TextField.jsx
@@ -1,8 +1,18 @@
 const TextField = ({ label, name, value, onChange }) => {
   return (
     <div>
-      <label htmlFor={name} className="block">{label}</label>
-      <input type="text" className="bg-gray-50 p-2 rounded-sm" name={name} id={name} value={value} onChange={onChange} />
+      <label htmlFor={name} className="block mb-4">
+        {label}
+      </label>
+      <input
+        type="text"
+        autoComplete="off"
+        className="textField__input"
+        name={name}
+        id={name}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
